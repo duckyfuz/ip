3487e0e130e761e60daacf37ab773b884dc2c92f
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-08-17 16:50:40 +0800

    Add rename, greet and exit functionality
    This change allows the bot to rename, greet, and exit. The bot name is also updated to Clanky, and is a static variable.

---
+++ src/main/java/Clanky.java
@@ -0,0 +1,13 @@
+public class Clanky {
+    static String bot_name = "Clanky";
+
+    public static void main(String[] args) {
+        String sep_line = "____________________________________________________________";
+        System.out.println(sep_line);
+        System.out.println("Hello! I'm " + bot_name + ".");
+        System.out.println("What can I do for you?");
+        System.out.println(sep_line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(sep_line);
+    }
+}
