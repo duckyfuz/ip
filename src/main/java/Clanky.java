f3c7893e00d34be8759d0e45145afc715d30c635
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-08-24 15:25:55 +0800

    Add mark and unmark functionality

--- src/main/java/Clanky.java
+++ src/main/java/Clanky.java
@@ -19,7 +19,7 @@ public class Clanky {
     private static void executeMainLoop() {
         String command;
         while (true) {
-            command = clankyScanner.nextLine();
+            command = clankyScanner.nextLine().trim();
 
             int status = handleCommand(command);
             if (status != 0) {
@@ -30,7 +30,18 @@ public class Clanky {
 
     private static int handleCommand(String command) {
         int status = 0;
-        switch (command) {
+
+        String[] commandParts = command.split(" ");
+        String action = commandParts[0];
+
+        int taskIndex;
+        try {
+            taskIndex = Integer.parseInt(commandParts[1]) - 1;
+        } catch (Throwable t) {
+            taskIndex = -1;
+        }
+
+        switch (action) {
         case "bye":
             status = 1;
             System.out.println(sep_line);
@@ -40,8 +51,28 @@ public class Clanky {
         case "list":
             System.out.println(sep_line);
             for (int i = 0; i < tasksCount; i++) {
-                System.out.println((i + 1) + ". " + tasks[i].getDescriptionWithStatusIcon());
+                System.out.println((i+1) + ". " + tasks[i].getDescriptionWithStatusIcon());
             }
+            System.out.println(sep_line);
+            break;
+        case "mark":
+            if (taskIndex == -1 || taskIndex >= tasks.length) {
+                handleInvalidCommand();
+                break;
+            }
+            tasks[taskIndex].markAsDone();
+            System.out.println(sep_line);
+            System.out.println("Nice! I've marked this task as done:\n" + tasks[taskIndex].getDescriptionWithStatusIcon());
+            System.out.println(sep_line);
+            break;
+        case "unmark":
+            if (taskIndex == -1 || taskIndex >= tasks.length) {
+                handleInvalidCommand();
+                break;
+            }
+            tasks[taskIndex].markAsNotDone();
+            System.out.println(sep_line);
+            System.out.println("Ok. I've marked this task as not done yet:\n" + tasks[taskIndex].getDescriptionWithStatusIcon());
             System.out.println(sep_line);
             break;
         default:
@@ -52,5 +83,11 @@ public class Clanky {
             System.out.println(sep_line);
         }
         return status;
+    }
+
+    private static void handleInvalidCommand() {
+        System.out.println(sep_line);
+        System.out.println("I don't understand you and it's your fault. Try again.");
+        System.out.println(sep_line);
     }
 }
