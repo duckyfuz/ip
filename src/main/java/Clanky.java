cf96fa818a55331c9d62f32b5709fa68133c86ef
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-08-24 13:34:03 +0800

    Add echo functionality
    Refactor to create executeMainLoop() and exitChat() methods.

--- src/main/java/Clanky.java
+++ src/main/java/Clanky.java
@@ -1,13 +1,35 @@
+import java.util.Scanner;
+
 public class Clanky {
     static String bot_name = "Clanky";
+    static String sep_line = "____________________________________________________________";
+    static Scanner clankyScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String sep_line = "____________________________________________________________";
         System.out.println(sep_line);
         System.out.println("Hello! I'm " + bot_name + ".");
         System.out.println("What can I do for you?");
         System.out.println(sep_line);
-        System.out.println("Bye. Hope to see you again soon!");
+        executeMainLoop();
+        exitChat();
+    }
+
+    private static void exitChat() {
         System.out.println(sep_line);
+        System.out.println("Bye! Don't come back.");
+        System.out.println(sep_line);
+    }
+
+    private static void executeMainLoop() {
+        String command;
+        while (true) {
+            command = clankyScanner.nextLine();
+            if (command.equals("exit")) {
+                break;
+            }
+            System.out.println(sep_line);
+            System.out.println(command);
+            System.out.println(sep_line);
+        }
     }
 }
