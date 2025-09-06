e6a1b8dfe8e771b136ae6dbd2ee95329d1a37558
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-09-06 15:33:52 +0800

    Update test script to reflect package changes

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Clanky.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/clanky/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
